c7ffc4eca567917e8f20a9c57f07b917786d390c
Sankalp <planetsankalp@gmail.com>
2024-02-21 21:17:37 +0530

    backend complete

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("product/<int:id>/", views.product, name="product"),
+    path("cart/", views.cart, name="cart"),
+    path("user/", views.user, name="user"),
+    path("search/<str:text>", views.search, name="search"),
+    path("add_to_cart/<product_id>", views.add_to_cart, name="add_to_cart"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,7 +3,32 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from app.models import User,Product,CartItem
+
+SANKALP = 1
 
 
 def index(request):
     return render(request, "index.html",{"thing": 20})
+
+def product(request, id):
+    response = "you are looking at the products Page"
+    product = Product.objects.get(id=id)
+    return render(request, "product.html",{"product": product})
+
+def cart(request):
+    cart = CartItem.objects.filter(user__exact=SANKALP)
+    return HttpResponse(cart)
+
+def user(request):
+    user = User.objects.get(id=SANKALP)
+    return render(request, "user.html",{"user": user})
+
+def search(request,text):
+    search = Product.objects.filter(name__icontains=text)
+    return render(request, "search.html",{"product_search_results": search})
+
+def add_to_cart(request, product_id):
+    cart_item = CartItem(user_id=SANKALP, product_id=product_id, quantity = 1)
+    cart_item.save()
+    return HttpResponse(cart_item.id)
